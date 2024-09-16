b33a4325f56bf4b7c0b327c95595e1a3de4bb1d3
alejandro <alejandrohurtadoalv@gmail.com>
2024-09-16 18:31:05 -0500

    Primer Commit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <>
+      <h1 className="text-white text-4xl">Hello, world!</h1>
+      <p>Welcome to your new single-spa application</p>
+    </>
+  )
+}
