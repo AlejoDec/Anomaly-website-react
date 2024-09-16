c29e27b85f85d1ed36bcce3fc6dd421f57475d07
alejandro <alejandrohurtadoalv@gmail.com>
2024-09-16 18:37:50 -0500

    Arreglo bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
     <>
-      <h1 className="text-white text-4xl">Hello, world!</h1>
+      <h1>Hello, world!</h1>
       <p>Welcome to your new single-spa application</p>
     </>
   )
