1ededef200a338a65b26ceb4fcee5b264068c74c
alejandro <alejandrohurtadoalv@gmail.com>
2024-10-05 12:23:42 -0500

    Update SEO 10/5/2024

--- src/components/general/Text1.tsx
+++ src/components/general/Text1.tsx
@@ -2,7 +2,7 @@ import { FormattedMessage } from 'react-intl';
 
 export default function Text1() {
     return (
-        <div className="hese flex flex-col items-center justify-center h-[40vh] overflow-hidden max-sm:w-full max-sm:font-base">
+        <div className="flex flex-col items-center justify-center h-[40vh] overflow-hidden max-sm:w-full max-sm:font-base">
             <p className="text-stone-300 text-4xl text-center max-sm:text-lg max-sm:w-screen max-sm:px-2">
                 <FormattedMessage id='text1' defaultMessage='En Anomaly, combinamos creatividad, innovación y tecnología para ofrecer soluciones personalizadas que se adaptan a las necesidades de tu negocio' />
             </p>
